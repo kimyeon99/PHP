947dafef08c008a5e3c6426321b3a8862c91466a
kimyeon99 <tmdwo7017@naver.com>
2021-06-30 19:24:18 +0900

    laravel - mariaDB

---
+++ 2021_06_30_070547_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->mediumText('content');
+            $table->string('image')->nullable();
+            $table->foreignID('user_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->timestamps(); //createat updateat 자동으로 만들어줌
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
