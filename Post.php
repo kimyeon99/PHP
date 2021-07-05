6fb51219c12c552ea0d0134fc41eebbebd1cf7b9
kimyeon99 <tmdwo7017@naver.com>
2021-07-05 20:32:32 +0900

    laravel web: model Post

---
+++ Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public function imagePath()
+    {
+        //   $path = '/storage/images';
+        $path = env('IMAGE_PATH', '/storage/images/');
+        $imageFile = $this->image ?? 'no_image.png';
+        return $path . $imageFile;
+    }
+}
